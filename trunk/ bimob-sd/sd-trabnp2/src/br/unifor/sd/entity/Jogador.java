ccbb9e1df88270a348d9dc02d30b905d91815874
tuliomonteazul@gmail.com <tuliomonteazul@gmail.com@31cfa124-48ef-e95f-edbd-5b55920ec495>
2011-05-06 22:00:01 +0000

    transferindo jogador

--- trunk/ bimob-sd/sd-trabnp2/src/br/unifor/sd/entity/Jogador.java
+++ trunk/ bimob-sd/sd-trabnp2/src/br/unifor/sd/entity/Jogador.java
@@ -1,5 +1,33 @@
 package br.unifor.sd.entity;
 
-public class Jogador {
-	private String name;
+import java.io.Serializable;
+import java.net.InetAddress;
+
+public class Jogador implements Serializable{
+	private String nome;
+	private InetAddress address;
+	
+	
+	public Jogador() {
+		super();
+	}
+	public Jogador(String nome, InetAddress address) {
+		super();
+		this.nome = nome;
+		this.address = address;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public InetAddress getAddress() {
+		return address;
+	}
+	public void setAddress(InetAddress address) {
+		this.address = address;
+	}
+	
+	
 }
